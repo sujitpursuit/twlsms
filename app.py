e4d132a99f09faf9a9df19398d20635e3e7c3237
Sujit Sarkar <sujit_s@pursuitsoftware.biz>
2024-06-01 15:55:37 -0400

    transfer stub

--- app.py
+++ app.py
@@ -165,5 +165,39 @@ def call_llm():
     else:
         return jsonify({"error": "prompt_llm not found"}), 400   
 
+
+@app.route("/dialog/transfer", methods=['POST'])
+def transfer_chat():
+
+    rcvd_data= request.data
+    print (f'=============RECEVIED \n {rcvd_data}')
+
+    json_data = request.get_json()
+    print (f'=============json data \n {json_data}')
+    print(f"========parameters {json_data['sessionInfo']['parameters']}")
+    # Check if the required field exists in the JSON data
+    if 'sessionInfo' in json_data and 'parameters' in json_data['sessionInfo'] and 'prompt_to_llm' in json_data['sessionInfo']['parameters']:
+        # Extract the "llm" field
+        prompt_llm = json_data['sessionInfo']['parameters']['prompt_to_llm']
+        print (f'Transfer to chat {prompt_llm}')
+        
+         # Create the WebhookResponse
+     
+        response = {
+            "fulfillment_response": {
+                "messages": [
+                    {
+                        "text": {
+                            "text": [f"I am transferring to chat"]
+                        }
+                    }
+                ]
+            }
+        }
+
+        return jsonify(response)
+    else:
+        return jsonify({"error": "prompt_llm not found"}), 400   
+    
 if __name__ == "__main__":
     app.run(debug=True)
